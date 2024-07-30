a5298c21b2a83c400e61fb77adc7ffdfd94e2411
Khuvit <161049966+Khuvit@users.noreply.github.com>
2024-07-30 14:19:34 +0900

    Update server.js connecting error fix

--- server.js
+++ server.js
@@ -3,12 +3,16 @@
 // localhostにDenoのHTTPサーバーを展開
 
 import { serveDir } from "https://deno.land/std@0.223.0/http/file_server.ts";
-let previousWord = "しりとり";
+let previousWord = "";
 
 Deno.serve(async (request) => {
   const pathname = new URL(request.url).pathname;
   console.log(`pathname: ${pathname}`);
-
+  if (request.method === "POST" && pathname === "/startGame") {
+    const requestJson = await request.json();
+    previousWord = requestJson.firstHiragana;
+    return new Response("Game started", { status: 200 });
+  }
   if (request.method === "GET" && pathname === "/shiritori") {
     return new Response(previousWord);
   }
@@ -17,14 +21,24 @@ Deno.serve(async (request) => {
     const requestJson = await request.json();
     const nextWord = requestJson.nextWord;
 
-    if (previousWord.slice(-1) === nextWord.slice(0, 1)) {
-      previousWord = nextWord;
-    }
-    else {
+    if (nextWord.slice(-1) === "ん") {
       return new Response(
         JSON.stringify({
-          "errorMessage": "前の単語に続いていません",
-          "errorCode": "10001"
+          errorMessage: "「ん」で終わる単語は使えません",
+          errorCode: "10000",
+        }),
+        {
+          status: 400,
+          headers: { "Content-Type": "application/json; charset=utf-8" },
+        }
+      );
+    } else if (previousWord.slice(-1) === nextWord.slice(0, 1)) {
+      previousWord = nextWord;
+    } else {
+      return new Response(
+        JSON.stringify({
+          errorMessage: "前の単語に続いていません",
+          errorCode: "10001",
         }),
         {
           status: 400,
